d50bda44c25637da7c0e7531ebadaa2c289ea58e
Ken Wiltshire <wiltshirek@gmail.com>
2024-11-01 16:11:19 -0400

    cleaning code for pull

--- test_neo4j.py
+++ test_neo4j.py
@@ -17,12 +17,12 @@ rag = LightRAG(
     working_dir=WORKING_DIR,
     llm_model_func=gpt_4o_mini_complete,  # Use gpt_4o_mini_complete LLM model
     kg="Neo4JStorage",
-    log_level="INFO"
+    log_level="DEBUG"
     # llm_model_func=gpt_4o_complete  # Optionally, use a stronger model
 )
 
-# with open("./book.txt") as f:
-#     rag.insert(f.read())
+with open("./book.txt") as f:
+    rag.insert(f.read())
 
 # Perform naive search
 print(rag.query("What are the top themes in this story?", param=QueryParam(mode="naive")))
